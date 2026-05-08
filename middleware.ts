42ab3982957f1b7c9871889ec95b2196302cafe0
System Fresh <systemfresh01@gmail.com>
2026-05-08 10:32:34 +0100

    signin

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ["/sign-in", "/sign-up"], 
+  // publicRoutes: ["/", "/signin*", "/signup*"],
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
